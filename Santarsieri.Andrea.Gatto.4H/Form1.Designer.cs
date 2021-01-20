92f10c8404da9db053f66d3079339d70474f24fd
andreasantarsieri <LAB113@INFO.RN>
2021-01-20 10:38:55 +0100

    Gatto

---
+++ Santarsieri.Andrea.Gatto.4H/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Santarsieri.Andrea.Gatto._4H
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnButton
+            // 
+            this.btnButton.Location = new System.Drawing.Point(1, 1);
+            this.btnButton.Name = "btnButton";
+            this.btnButton.Size = new System.Drawing.Size(75, 23);
+            this.btnButton.TabIndex = 0;
+            this.btnButton.Text = "Disegna";
+            this.btnButton.UseVisualStyleBackColor = true;
+            this.btnButton.Click += new System.EventHandler(this.btnButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnButton;
+    }
+}
+
---
+++ Santarsieri.Andrea.Gatto.4H/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Santarsieri.Andrea.Gatto._4H
+{
+    public partial class Form1 : Form
+    {
+        const int LATO_X = 200;
+        const int LATO_Y = 300;
+
+        const int MARGINE_SINISTRO = 100;
+        const int MARGINE_DESTRO = 100;
+        const int MARGINE_SUPERIORE = 100;
+        const int MARGINE_INFERIORE = 100;
+
+
+        const int LATO_X_FORM = (MARGINE_SINISTRO + LATO_X + MARGINE_DESTRO);
+        const int LATO_Y_FORM = (MARGINE_SUPERIORE + LATO_Y + MARGINE_INFERIORE);
+        public Form1()
+        {
+            InitializeComponent();
+            this.MinimumSize = new Size(LATO_X_FORM, LATO_Y_FORM);
+            this.MaximumSize = new Size(LATO_X_FORM, LATO_Y_FORM);
+        }
+
+        private int X(double x)
+        {
+            int xLogico = 0;
+            xLogico = (int)(x) + MARGINE_SINISTRO;
+            return xLogico;
+        }
+
+        private int Y(double y)
+        {
+            int yLogico = 0;
+            yLogico = (MARGINE_SUPERIORE + LATO_Y - (int)(y));
+            return yLogico;
+        }
+
+        private void btnButton_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            Graphics dc = this.CreateGraphics();
+            Pen BlackPen = new Pen(Color.Black, 1);
+            Pen BluePen = new Pen(Color.Blue, 1);
+
+            dc.DrawRectangle(BluePen, MARGINE_SINISTRO + 0, MARGINE_SUPERIORE + 0, LATO_X, LATO_Y);
+
+            Punto[] p = new Punto[39];
+
+            p[0] = new Punto(20, 0);
+            p[1] = new Punto(40, 0);
+            p[2] = new Punto(60, 20);
+            p[3] = new Punto(40, 0);
+            p[4] = new Punto(60, 0);
+            p[5] = new Punto(80, 20);
+            p[6] = new Punto(100, 20);
+            p[7] = new Punto(120, 20);
+            p[8] = new Punto(140, 20);
+            p[9] = new Punto(140, 0);
+            p[10] = new Punto(120, 20);
+            p[11] = new Punto(140, 20);
+            p[12] = new Punto(160, 00);
+            p[13] = new Punto(160, 20);
+            p[14] = new Punto(140, 40);
+            p[15] = new Punto(140, 60);
+            p[16] = new Punto(160, 60);
+            p[17] = new Punto(180, 40);
+            p[18] = new Punto(180, 60);
+            p[19] = new Punto(160, 80);
+            p[20] = new Punto(140, 80);
+            p[21] = new Punto(120, 60);
+            p[22] = new Punto(140, 40);
+            p[23] = new Punto(120, 60);
+            p[24] = new Punto(100, 60);
+            p[25] = new Punto(80, 60);
+            p[26] = new Punto(80, 80);
+            p[27] = new Punto(80, 100);
+            p[28] = new Punto(60, 80);
+            p[29] = new Punto(40, 80);
+            p[30] = new Punto(20, 100);
+            p[31] = new Punto(20, 80);
+            p[32] = new Punto(20, 60);
+            p[33] = new Punto(40, 40);
+            p[34] = new Punto(60, 40);
+            p[35] = new Punto(80, 60);
+            p[36] = new Punto(60, 40);
+            p[37] = new Punto(40, 40);
+            p[38] = new Punto(40, 20);
+
+
+            for (i = 0; i < 39; i++)
+            {
+                int j = i + 1;
+                if (j == 39) j = 0;
+                dc.DrawLine(BlackPen, X(p[i].X), Y(p[i].Y), X(p[j].X), Y(p[j].Y));
+                
+            }
+
+
+        }
+    }
+}
